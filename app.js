15bc424498da42c9b5a51bca5c60c5b92e6fae9f
hakim fatmi <126871854+GLcoz@users.noreply.github.com>
2024-06-26 01:20:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('ENSI in the Cloud');
+  res.send('Ensi in the Cloud');
 });
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
+app.listen(port, '::', () => {
+  console.log(`App listening on port ${port}`);
 });
